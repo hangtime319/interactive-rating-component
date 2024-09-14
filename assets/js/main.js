9db39487a863b81f8ae40ec2d0834f9f11a22fd6
hangtime319 <hangtime31@hotmail.com>
2024-09-14 19:14:01 -0300

    Alterando README.md

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Elements
+// Elements HTML.
 const contentRating = document.getElementById("content-rating");
 const thanksState = document.querySelector(".thanks");
 const buttons = document.querySelectorAll("#rating button");
@@ -10,7 +10,10 @@ let selectedRating = null;
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
+        // Remove active class from previously selected button
         buttons.forEach(btn => btn.classList.remove('active'));
+
+        // Set selected rating and add active class to clicked button
         selectedRating = button.value;
         button.classList.add('active');        
     });
@@ -23,6 +26,7 @@ submitButton.addEventListener('click', () => {
         contentRating.classList.add('display-none');
         thanksState.classList.remove('display-none')
     } else {
+        // Handle case where no rating is selected (optional)
         alert('You should select a rating before submitting!')
     }
 });
